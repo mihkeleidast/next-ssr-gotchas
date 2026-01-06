de324d84425b7be77b3ec88bb583dabf976291c7
Mihkel Eidast <mihkel@eida.st>
2026-01-06 19:22:06 +0200

    fix ssr

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,11 +2,13 @@
 import React from 'react';
 
 function Counter() {
-  const [count, setCount] = React.useState(() => {
-    return Number(
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    setCount(Number(
       window.localStorage.getItem('saved-count') || 0
-    );
-  });
+    ))
+  }, [])
 
   React.useEffect(() => {
     window.localStorage.setItem('saved-count', count);
